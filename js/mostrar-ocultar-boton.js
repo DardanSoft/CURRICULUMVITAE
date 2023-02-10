bd4392724b0e1b0985113b53c15a0f088d100db2
Dante <dantesese@hotmail.com>
2023-02-10 19:13:34 -0300

    Primer escritura

---
+++ js/mostrar-ocultar-boton.js
@@ -0,0 +1,27 @@
+
+
+function mostraryocultaruno() {
+    var x = document.getElementById("datosuno");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function mostraryocultardos() {
+    var x = document.getElementById("datosdos");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mostraryocultartres() {
+    var x = document.getElementById("datostres");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
